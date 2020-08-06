46cb9d8c2c6bdba879406b8a8009f81c67db21e5
Mejri10 <66725886+Mejri10@users.noreply.github.com>
2020-08-06 10:32:00 +0100

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Fullname from './profils/FullName';
+import Profilphoto from './profils/ProfilPhoto';
+import Adress from './profils/Adress';
+
+
+const Main = () => {
+    return (
+      <div>
+        <Fullname />
+        <Profilphoto />
+        <Adress />
+
+      </div>
+    );
+   };
+   
+   export  default Main;
